d53ef2b0306dedc24ecad02daebebee23422fbd7
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2016-05-18 00:03:47 +0200

    New states covered with templates, 404 handle

--- web/js/app.js
+++ web/js/app.js
@@ -9,16 +9,39 @@ angular.module('sogo', [
     'sogo.controllers'
 ])
 
-.config(function ($stateProvider, $httpProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.when('', '/login');
+
+    $urlRouterProvider.otherwise(function($injector, $location) {
+        $injector.invoke(['$state', function($state) {
+            $state.go('404');
+        }]);
+    }); 
 
     $stateProvider
+        .state('404', {
+            templateUrl: 'partials/404.html'
+        })
         .state('greeting', {
             url: '/greeting',
             templateUrl: 'partials/views/greeting.html',
             controller: 'GreetingController',
             service: 'GreetingService'
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'partials/views/login.html'
+        })
+        .state('signup', {
+            url: '/signup',
+            templateUrl: 'partials/views/signup.html'
+        })
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partials/views/home.html'
         });
 
 }).run(function ($state) {
-   $state.go('greeting');
+   $state.go('login');
 });
--- web/js/directives.js
+++ web/js/directives.js
@@ -2,4 +2,22 @@
 
 /* Directives */
 
-var directives = angular.module('sogo.directives', []);
+angular.module('sogo.directives', [])
+
+.directive('inFooter', [
+    function () {
+        return {
+            restrict: 'E',
+            scope: {
+                user: '='
+            },
+            templateUrl: '/partials/directives/in_footer.html',
+            link: function ($scope) {
+                $scope.$watch('user', function () {
+                    $scope.showData = ($scope.user != undefined);
+                });
+
+            }
+        }
+    }
+]);
