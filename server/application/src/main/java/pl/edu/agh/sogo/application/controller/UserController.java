23969407490f06decc0d53ee7f6558603443da9a
Nesbite <grzegorz.siatka@gmail.com>
2016-05-21 19:30:19 +0200

    Enabled CORS in controllers

--- server/application/src/main/java/pl/edu/agh/sogo/application/controller/UserController.java
+++ server/application/src/main/java/pl/edu/agh/sogo/application/controller/UserController.java
@@ -24,8 +24,8 @@ public class UserController {
 
     @ResponseBody
     @RequestMapping(value = "/{username}", method = RequestMethod.GET)
-    public User getUser(@PathVariable(value = "username") String username) {
-        return userService.findUserByUsername(username);
+    public ResponseEntity<User> getUser(@PathVariable(value = "username") String username) {
+        return new ResponseEntity<>(userService.findUserByUsername(username), HttpStatus.OK);
     }
 
     @ResponseBody
