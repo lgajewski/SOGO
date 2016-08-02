d82a1861210f52b81b367683553f74f5aa0db98b
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2016-08-02 23:19:48 +0200

    Raw SOGO web application into template

--- src/main/webapp/app/app.constants.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    'use strict';
-    angular
-        .module('jhipsterApp')
-        .constant('VERSION', "0.0.1")
-        .constant('DEBUG_INFO_ENABLED', true)
-;
-})();
---
+++ src/main/webapp/js/services.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('sogo.services',[])
+
+    .factory('GreetingService',function($resource) {
+        return $resource('http://rest-service.guides.spring.io/greeting');
+    });
+
+
