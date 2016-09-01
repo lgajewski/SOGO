eade719c4e2275a8007fdb627bb4c112204875ba
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2016-09-02 00:36:34 +0200

    Added sample UI test with travis configuration

---
+++ src/test/javascript/protractor.conf.js
@@ -0,0 +1,21 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
+exports.config = {
+    suites: {
+        auth: './protractor/*.js'
+    },
+
+    framework: 'jasmine2',
+
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000
+    },
+
+    onPrepare: function () {
+        browser.driver.manage().window().setSize(1280, 1024);
+        jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
+            savePath: 'build/reports/e2e/'
+        }));
+    }
+};
