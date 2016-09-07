4b6e9183ff79111f1b049ec48ecc8d1869e5f3f4
Nesbite <grzegorz.siatka@gmail.com>
2016-09-07 11:10:44 +0200

    Loading elements from database and displaying their locations added

--- src/main/webapp/app/view/trucks/trucks.controller.js
+++ src/main/webapp/app/view/trucks/trucks.controller.js
@@ -3,11 +3,11 @@
 
     angular
         .module('sogo')
-        .controller('UsersController', TrucksController);
+        .controller('TrucksController', TrucksController);
 
-    TrucksController.$inject = ['$scope', '$filter', 'Restangular', 'ActiveItemService'];
+    TrucksController.$inject = ['$scope', 'Restangular', 'ActiveItemService'];
 
-    function TrucksController($scope, $filter, Restangular, ActiveItemService) {
+    function TrucksController($scope, Restangular, ActiveItemService) {
 
         $scope.activeObject = ActiveItemService.getObject();
         $scope.setActiveObject = function (item) {
@@ -21,7 +21,7 @@
         $scope.getTrucks = function () {
             Restangular.all('trucks').getList().then(function (data) {
                 $scope.items = data;
-                $scope.search();
+                console.log(data);
             })
         };
         $scope.getTrucks();
@@ -33,82 +33,13 @@
         };
 
         $scope.showDetail = function (item) {
-            if ($scope.active != item.registration) {
-                $scope.active = item.registration;
+            if ($scope.active != item.id) {
+                $scope.active = item.id;
             }
             else {
                 $scope.active = null;
             }
         };
-
-
-        $scope.gap = 5;
-
-        $scope.filteredItems = [];
-        $scope.groupedItems = [];
-        $scope.itemsPerPage = 5;
-        $scope.pagedItems = [];
-        $scope.currentPage = 0;
-
-
-        // init the filtered items
-        $scope.search = function () {
-
-            $scope.currentPage = 0;
-            // now group by pages
-            $scope.groupToPages();
-        };
-
-        // calculate page in place
-        $scope.groupToPages = function () {
-            $scope.pagedItems = [];
-
-            for (var i = 0; i < $scope.items.length; i++) {
-                if (i % $scope.itemsPerPage === 0) {
-                    $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)] = [$scope.items[i]];
-                } else {
-                    $scope.pagedItems[Math.floor(i / $scope.itemsPerPage)].push($scope.items[i]);
-                }
-            }
-        };
-
-        $scope.range = function (size, start, end) {
-            var ret = [];
-            console.log(size, start, end);
-
-            if (size < end) {
-                end = size;
-                if (size < $scope.gap) {
-                    start = 0;
-                } else {
-                    start = size - $scope.gap;
-                }
-
-            }
-            for (var i = start; i < end; i++) {
-                ret.push(i);
-            }
-            console.log(ret);
-            return ret;
-        };
-
-        $scope.prevPage = function () {
-            if ($scope.currentPage > 0) {
-                $scope.currentPage--;
-            }
-        };
-
-        $scope.nextPage = function () {
-            if ($scope.currentPage < $scope.pagedItems.length - 1) {
-                $scope.currentPage++;
-            }
-        };
-
-        $scope.setPage = function () {
-            $scope.currentPage = this.n;
-        };
-
-        // functions have been describe process the data for display
 
     }
 
