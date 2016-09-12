2a5b914477afcd04c58329c4be7b5c23126eb89d
Patrycja Wrona <patrycjawrona23@gmail.com>
2016-09-12 21:28:10 +0200

    Page lenghtening script optimized

--- src/main/webapp/assets/js/load_css.js
+++ src/main/webapp/assets/js/load_css.js
@@ -1,25 +1,27 @@
-$(document).ready(function () {
-    var topOffset = 50;
-    var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-    if (width < 768) {
-        $('div.navbar-collapse').addClass('collapse');
-        topOffset = 100; // 2-row-menu
-    } else {
-        $('div.navbar-collapse').removeClass('collapse');
-    }
+$(function() {
+    angular.element('[ng-app=sogo]').scope().$on('onNavbarLoaded', function() {
+        var topOffset = 50;
+        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        if (width < 768) {
+            $('div.navbar-collapse').addClass('collapse');
+            topOffset = 100; // 2-row-menu
+        } else {
+            $('div.navbar-collapse').removeClass('collapse');
+        }
 
-    var height = ((window.innerHeight > 0) ? window.innerHeight : screen.height) - 1;
-    height = height - topOffset;
-    if (height < 1) height = 1;
-    if (height > topOffset) {
-        $("#page-wrapper").css("min-height", (height) + "px");
-    }
+        var height = ((window.innerHeight > 0) ? window.innerHeight : screen.height) - 1;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+            $("#page-wrapper").css("min-height", (height) + "px");
+        }
 
-    $("ul.side-nav li:first").addClass("active").show();
-    $("ul.side-nav li").click(function() {
-        $("ul.side-nav li").removeClass("active");
-    });
-    $(".navbar-brand").click(function() {
-        $("ul.side-nav li:first").addClass("active");
+        $("ul.side-nav li:first").addClass("active").show();
+        $("ul.side-nav li").click(function() {
+            $("ul.side-nav li").removeClass("active");
+        });
+        $(".navbar-brand").click(function() {
+            $("ul.side-nav li:first").addClass("active");
+        });
     });
 });
