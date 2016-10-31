42961f017e42762529e8d2b601db25f8a6d284c9
Zuzanna Chrząstek <zuzanna.chrzastek@gmail.com>
2016-10-31 17:29:22 +0100

    'Hamburger menu not hiding after selecting page' bug fixed

--- src/main/webapp/app/view/navbar/navbar.controller.js
+++ src/main/webapp/app/view/navbar/navbar.controller.js
@@ -20,6 +20,13 @@
             $scope.$emit('onNavbarLoaded');
         });
 
+        $(function () {
+            var navMain = $("#nav-main");
+            navMain.on("click", "a", null, function () {
+                navMain.collapse('hide');
+            });
+        });
+
         function configureSse() {
             var onContainerUpdated = function (event) {
                 var updatedContainer = JSON.parse(event.data);
