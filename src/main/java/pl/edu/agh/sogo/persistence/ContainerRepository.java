81c09dc620d41d5659f42b50b578fcd331d38772
Nesbite <grzegorz.siatka@gmail.com>
2016-11-16 06:48:55 +0100

    Add 'repair container' functionality

--- src/main/java/pl/edu/agh/sogo/persistence/ContainerRepository.java
+++ src/main/java/pl/edu/agh/sogo/persistence/ContainerRepository.java
@@ -3,9 +3,11 @@ package pl.edu.agh.sogo.persistence;
 import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import pl.edu.agh.sogo.domain.Container;
+import pl.edu.agh.sogo.domain.User;
 
 import java.util.List;
 
 public interface ContainerRepository extends MongoRepository<Container, String> {
     List<Container> findByLocationNear(Point point);
+    List<Container> findAllByRepairer(User user);
 }
