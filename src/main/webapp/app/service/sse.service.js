f605effb3a1ca0d1ddaa60f6f0cbac89a05b774c
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2016-11-22 15:37:37 +0100

    Handle closed connection on client-side

--- src/main/webapp/app/service/sse.service.js
+++ src/main/webapp/app/service/sse.service.js
@@ -5,6 +5,13 @@
         .module('sogo')
         .factory('SseService', SseService);
 
+    class SseEntry {
+        constructor(eventSource, callback) {
+            this.eventSource = eventSource;
+            this.callback = callback;
+        }
+    }
+
     function SseService() {
         var cache = [];
 
@@ -14,28 +21,23 @@
         };
 
         function register(eventName, callback) {
-            var index = cache.map(e => e.cb).indexOf(callback);
+            var index = cache.map(entry => entry.callback).indexOf(callback);
             if (index >= 0) {
                 console.error("Unable to register. This callback has been already registered.")
             } else {
                 var source = new EventSource("/api/sse");
-
                 source.addEventListener(eventName, callback);
-                source.onerror = event => console.log(event);
-                source.onopen = event => console.log(event);
-
-                cache.push({
-                    source: source,
-                    cb: callback
-                })
+                
+                cache.push(new SseEntry(source, callback));
             }
         }
 
         function unregister(callback) {
-            var index = cache.map(e => e.cb).indexOf(callback);
+            var index = cache.map(entry => entry.callback).indexOf(callback);
             if (index >= 0) {
-                var elem = cache.splice(index, 1)[0];
-                elem.source.close();
+                var entry = cache.splice(index, 1)[0];
+                entry.callback = angular.noop();
+                entry.eventSource.onerror = () => entry.eventSource.close();
             } else {
                 console.error("Unable to unregister SseService. This callback is not registered.")
             }
