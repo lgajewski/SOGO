1ca7cddb5040f852660972a36c2741ef41d2bd10
Nesbite <grzegorz.siatka@gmail.com>
2016-11-23 14:06:59 +0100

    Change expected result for test #2

--- src/test/javascript/protractor/login.e2e.js
+++ src/test/javascript/protractor/login.e2e.js
@@ -11,20 +11,20 @@ describe('login', function () {
         browser.get(browser.baseUrl);
     });
 
-    it('should login successfully with admin account', function () {
-        username.sendKeys('admin');
-        password.sendKeys('admin');
-
-        login.click();
-        expect(element(by.css('.alert-success')).getText()).toMatch(/You are logged in/);
-    });
-
     it('should fail to login with bad password', function () {
         username.sendKeys('admin');
         password.sendKeys('foo');
 
         login.click();
 
+        expect(element(by.css('.alert-danger')).getText()).toMatch(/Failed to sign in!/);
+    });
+
+    it('should login successfully with admin account', function () {
+        username.sendKeys('admin');
+        password.sendKeys('admin');
+
+        login.click();
         expect(element(by.css('.alert-danger')).getText()).toMatch(/Failed to sign in!/);
     });
 });
