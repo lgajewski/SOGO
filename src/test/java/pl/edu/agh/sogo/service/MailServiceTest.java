1928bd29eee1ba25d888783af1fa9b89d1e257e6
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2016-12-11 23:52:13 +0100

    Remove unused imports in MailServiceTest

--- src/test/java/pl/edu/agh/sogo/service/MailServiceTest.java
+++ src/test/java/pl/edu/agh/sogo/service/MailServiceTest.java
@@ -1,25 +1,17 @@
 package pl.edu.agh.sogo.service;
 
-import com.sun.org.apache.xpath.internal.Arg;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.context.MessageSource;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.thymeleaf.context.Context;
-import org.thymeleaf.context.VariablesMap;
 import org.thymeleaf.spring4.SpringTemplateEngine;
-import pl.edu.agh.sogo.domain.User;
 
 import javax.mail.internet.MimeMessage;
-import java.util.Collections;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
