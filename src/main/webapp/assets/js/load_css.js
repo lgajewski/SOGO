549bdcb84aca2c30067cfabe3585f4878f826be9
Lukasz Gajewski <lukasz.gajewski94@gmail.com>
2016-12-27 23:20:47 +0100

    Improve navbar collor to be more contrastable

--- src/main/webapp/assets/js/load_css.js
+++ src/main/webapp/assets/js/load_css.js
@@ -1,13 +1,20 @@
-$(function() {
-    angular.element('[ng-app=sogo]').scope().$on('onNavbarLoaded', function() {
-        var topOffset = 50;
+$(function () {
+    angular.element('[ng-app=sogo]').scope().$on('onNavbarLoaded', function () {
         var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
         if (width < 768) {
             $('div.navbar-collapse').addClass('collapse');
-            topOffset = 100; // 2-row-menu
         } else {
             $('div.navbar-collapse').removeClass('collapse');
         }
+
+        adjustMinHeight();
+    });
+
+    angular.element(window).bind('resize', adjustMinHeight);
+
+    function adjustMinHeight() {
+        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        var topOffset = (width < 768) ? 100 : 50;
 
         var height = ((window.innerHeight > 0) ? window.innerHeight : screen.height) - 1;
         height = height - topOffset;
@@ -15,13 +22,12 @@ $(function() {
         if (height > topOffset) {
             $("#page-wrapper").css("min-height", (height) + "px");
         }
+    }
 
-    });
-
-    angular.element('[ng-app=sogo]').scope().$on('$stateChangeSuccess', function(event, toState) {
+    angular.element('[ng-app=sogo]').scope().$on('$stateChangeSuccess', function (event, toState) {
         $('ul.side-nav a').each(function (i, el) {
             var parent = $(this).parent();
-            if(el.href.includes(toState.name)) {
+            if (el.href.includes(toState.name)) {
                 parent.addClass("active");
             } else {
                 parent.removeClass("active");
